95f9e1e034d8d24f964db98776728b3a36e55015
ankurkoul <er.ankurkoul@gmail.com>
2021-05-06 07:00:05 +0530

    Spring-bootwith web flux

---
+++ src/main/java/com/springflux/tester/springfluxtester/SpringfluxtesterApplication.java
@@ -0,0 +1,13 @@
+package com.springflux.tester.springfluxtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfluxtesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfluxtesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springflux/tester/springfluxtester/SpringfluxtesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springflux.tester.springfluxtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfluxtesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
